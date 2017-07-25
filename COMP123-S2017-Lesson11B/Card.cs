2a96545f79f7f384d5c961d7fb0d44ee50109f7c
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-07-25 17:13:42 -0400

    Added Card class

---
+++ COMP123-S2017-Lesson11B/Card.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Tom Tsiliopoulos
+ * Date: July 25, 2017
+ * Description: This is the Card Class
+ * Version: 0.1 - Created the Card class
+ */
+
+namespace COMP123_S2017_Lesson11B
+{
+    public class Card
+    {
+        // PRIVATE INSTANCE VARIABLES
+        private Face _face;
+        private Suit _suit;
+
+        // PUBLIC PROPERTIES
+        public Face Face {
+
+            get
+            {
+                return this._face;
+            }
+
+            set
+            {
+                this._face = value;
+            }
+        }
+
+        public Suit Suit {
+
+            get
+            {
+                return this._suit;
+            }
+
+
+            set
+            {
+                this._suit = value;
+            }
+
+        }
+
+        // CONSTRUCTORS
+
+        /// <summary>
+        /// This is the main constructor.
+        /// It takes two parameters - face (Face) - suit (Suit)
+        /// </summary>
+        /// <param name="face"></param>
+        /// <param name="suit"></param>
+        public Card(Face face, Suit suit)
+        {
+            this.Face = face;
+            this.Suit = suit;
+        }
+
+        // PRIVATE METHODS
+
+        // PUBLIC METHODS
+    }
+}
